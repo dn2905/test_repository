a98ec6499a8bf21fa17c4aade8dc2ce727b8b796
Den F <dn2905python@gmail.com>
2022-02-17 09:08:05 +0200

    Add questions

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,46 @@
 "use strict";
-let helloName = prompt("What's you name?");
-alert(`Hello, ${helloName}!`);
+
+let countOfFilms;
+let tr = true;
+do {
+  countOfFilms = prompt("Сколько фильмов вы посмотрели?", "");
+  if (countOfFilms != null && !!Number(countOfFilms)) {
+    countOfFilms = +countOfFilms;
+    tr = false;
+  }
+  console.log(countOfFilms, tr, Number(countOfFilms));
+} while (tr);
+
+const personalMovieDB = {
+  count: countOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+for (let i = 0; i < 2; i++) {
+  let lastF = prompt("Назовите последний просмотренный фильм"),
+    r = prompt("Оцените его");
+  if (
+    lastF != "" &&
+    r != "" &&
+    lastF != null &&
+    r != null &&
+    lastF.length < 50
+  ) {
+    personalMovieDB.movies[lastF] = r;
+  } else {
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  alert("Маловато..");
+} else if (30 < personalMovieDB.count > 10) {
+  alert("Вы стандартный зритель");
+} else if (personalMovieDB.count > 30) {
+  alert("Да вы киноман!");
+}
+
+console.log(personalMovieDB);
